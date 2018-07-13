6a68b3f621407e2225f33ce696c0a213dd2efa13
jelockwood <jelockwood@gmail.com>
2018-07-13 15:10:51 +0100

    Add files via upload

---
+++ installer_package/preinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# pinpoint 3.x pre-install script
+# Removes any previous copy
+if [ -f "/Library/LaunchDaemons/com.clburlison.pinpoint.plist" ]; then
+	/bin/launchctl unload "/Library/LaunchDaemons/com.clburlison.pinpoint.plist"
+	/bin/rm "/Library/LaunchDaemons/com.clburlison.pinpoint.plist"
+fi
+if [ -d "/Library/Application Support/pinpoint" ]; then
+	/bin/rm -r "/Library/Application Support/pinpoint"
+fi
+exit
