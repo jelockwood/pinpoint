12f109020d50e37f35fe3ded1d7a156349b0640d
jelockwood <jelockwood@gmail.com>
2018-07-18 09:55:49 +0100

    Create pinpoint_attribute.sh

---
+++ pinpoint_attribute.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script reads the latest GoogleMap URL produced by pinpoint.sh on a client Mac and returns it as an extension attribute
+if [ ! -e "/Library/Application Support/pinpoint/location.plist" ]; then
+	exit 1
+else
+	url=`/usr/bin/defaults read "/Library/Application Support/pinpoint/location" GoogleMap`
+	echo "<result>$url</result>"
+fi
+exit 0
