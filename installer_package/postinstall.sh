9b344308000ebbcbeb7bd1d4c187a6eace59460c
jelockwood <jelockwood@gmail.com>
2019-02-20 13:26:35 +0000

    Fix default value for USE_GECODE preference

--- installer_package/postinstall.sh
+++ installer_package/postinstall.sh
@@ -5,6 +5,7 @@
 /usr/sbin/chown root:wheel "/Library/LaunchDaemons/com.jelockwood.pinpoint.plist"
 if [ ! -f "/Library/Preferences/com.jelockwood.pinpoint.plist" ]; then
 	/usr/bin/defaults write "/Library/Preferences/com.jelockwood.pinpoint" USE_ALTITUDE -bool FALSE
+	/usr/bin/defaults write "/Library/Preferences/com.jelockwood.pinpoint" USE_GEOCODE -bool TRUE
 	/usr/bin/defaults write "/Library/Preferences/com.jelockwood.pinpoint" YOUR_API_KEY -string ""
 fi
 /bin/launchctl load "/Library/LaunchDaemons/com.jelockwood.pinpoint.plist"
