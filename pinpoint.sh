b99cfb7fc990580a573e578134ec73edae1dbe6a
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2020-10-19 17:48:52 -0500

    Bug fixes
    • Quoted "$resultcode" to prevent `line 175: [: too many arguments` error
    
    • Changed LS_Enabled from a boolean to an integer to fix MunkiReport error:
    `Server Error: SQLSTATE[HY000]: General error: 1366 Incorrect integer value: '' for column 'ls_enabled' at row 1`
    
    See @bochoven comment, "The database implementation that MR uses does not have boolean as a storage type" in discussion at https://macadmins.slack.com/archives/C066NK3K5/p1580221740182600\

--- pinpoint.sh
+++ pinpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright John E. Lockwood (2018-2019)
+# Copyright John E. Lockwood (2018-2020)
 #
 # pinpoint a script to find your Mac's location
 #
@@ -10,7 +10,7 @@
 # Script name
 scriptname=$(basename -- "$0")
 # Version number
-versionstring="3.0.3"
+versionstring="3.0.4"
 # get date and time in UTC hence timezone offset is zero
 rundate=`date -u +%Y-%m-%d\ %H:%M:%S\ +0000`
 #echo "$rundate"
@@ -172,7 +172,7 @@ echo "$result"
 # A 400 or 404 error might mean none of your detect WiFi BSSIDs are known to Google
 resultcode=`echo "$result" | grep "HTTP" | awk '{print $2}'`
 echo "Result code = $resultcode"
-if [ $resultcode != "200" ]; then
+if [ "$resultcode" != "200" ]; then
 	if [ -e "$resultslocation" ]; then
 		reason=`echo "$result" | grep "HTTP" | awk -F ": " '{print $2}'`
 		defaults write "$resultslocation" CurrentStatus -string "Error $resultcode - $reason"
@@ -258,9 +258,9 @@ else
 		ls_enabled=`defaults read "/var/db/locationd/Library/Preferences/ByHost/com.apple.locationd" LocationServicesEnabled`
 		echo "ls_enabled = $ls_enabled"
 		if [ "$ls_enabled" == "True" ]; then
-			defaults write "$resultslocation" LS_Enabled -bool TRUE
+			defaults write "$resultslocation" LS_Enabled -int 1
 		else
-			defaults write "$resultslocation" LS_Enabled -bool FALSE
+			defaults write "$resultslocation" LS_Enabled -int 0
 		fi
 		defaults write "$resultslocation" LastLocationRun -string "$rundate"
 		defaults write "$resultslocation" LastRun -string "$rundate"
