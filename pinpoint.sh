e71263fb9f39ccfe9981f75aa5be035ea3094d07
jelockwood <jelockwood@gmail.com>
2024-02-20 09:49:02 +0000

    Update pinpoint.sh
    Updated version number to include patch from Ofir

--- pinpoint.sh
+++ pinpoint.sh
@@ -19,7 +19,7 @@
 # Script name
 scriptname=$(basename -- "$0")
 # Version number
-versionstring="3.2.4"
+versionstring="3.2.5"
 # get date and time in UTC hence timezone offset is zero
 rundate=`date -u +%Y-%m-%d\ %H:%M:%S\ +0000`
 #echo "$rundate"
