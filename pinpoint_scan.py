91fe1b6aa49667730df3dc951dbe2506fdf62abd
jelockwood <jelockwood@gmail.com>
2024-03-15 19:15:32 +0000

    Update pinpoint_scan.py

--- pinpoint_scan.py
+++ pinpoint_scan.py
@@ -1,5 +1,26 @@
 #!/usr/local/bin/managed_python3
 
+import CoreLocation
+from time import sleep
+
+location_manager = CoreLocation.CLLocationManager.alloc().init()
+location_manager.startUpdatingLocation()
+
+max_wait = 60
+# Get the current authorization status for Python
+for i in range(1, max_wait):
+    authorization_status = location_manager.authorizationStatus()
+    if authorization_status == 3 or authorization_status == 4:
+#        print("Python has been authorized for location services")
+        break
+    if i == max_wait-1:
+        exit("Unable to obtain authorization, exiting")
+    sleep(1)
+
+# coord = location_manager.location().coordinate()
+# lat, lon = coord.latitude, coord.longitude
+# print("Your location is %f, %f" % (lat, lon))
+
 import objc
 objc.loadBundle(
     "CoreWLAN",
